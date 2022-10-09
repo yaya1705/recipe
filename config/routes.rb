a8ce6dac63fc014fa893ffc39542549cbea24816
yaya1705 <yaya0517k@gmail.com>
2022-10-09 14:04:28 +0000

    [first]routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  get 'menus/index'
+  root to: 'menus#index'
+  resources :menus, only:[:index,:create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
