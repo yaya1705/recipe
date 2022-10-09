fcaf0fcbe49036afb64133859c99f5545692f2cd
yaya1705 <yaya0517k@gmail.com>
2022-10-09 14:41:14 +0000

    [ffirst]投稿機能

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,16 +1,16 @@
 class MenusController < ApplicationController
-  
+
   def index
     @menus = Menu.all
     @menu = Menu.new
   end
-  
+
   def create
     @menu = Menu.new(menu_params)
     @menu.save
-    redirect_to :index
+    redirect_to root_path
   end
-  
+
   def menu_params
     params.require(:menu).permit(:title,:body)
   end
